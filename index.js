132c1584f04e01db01d4ab72092592e2b3a1a9eb
Niharika Santosh Gadhave <gadhaven1@udayton.edu>
2022-02-10 16:35:27 +0000

    proejct file updated

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+var port = process.env.PORT || 8080;
+app.use (express.urlencoded ({extended: false}))
+const cors = require('cors')//New for microservice
+app.use(cors())//New for microservice
+app.listen(port)
+    console.log("Express server is running on port " + port)
+
+app.get('/', (req, res) => {
+    res.send('Microservice Gateway by Niharika Gadhave');
+})
