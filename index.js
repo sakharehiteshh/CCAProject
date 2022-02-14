44e6ebc899044f517bc12bddea887946044b7a9a
Niharika Santosh Gadhave <gadhaven1@udayton.edu>
2022-02-14 10:36:36 +0000

    updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use (express.urlencoded ({extended: false}))
 const cors = require('cors')//New for microservice
 app.use(cors())//New for microservice
 app.listen(port)
-    console.log("Express server is running on port " + port)
+    console.log("/Usage: host/Age_ChineseZodiac?year=2000 " + port)
 app.get('/', (req, res) => {
     res.send('You are 21 year olds and you are a Dragon');
 
