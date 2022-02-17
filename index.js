3631b1f56388135d7df3db40b923a31633a89497
Hitesh Vilas Sakhare <sakhareh1@udayton.edu>
2022-02-17 06:06:49 +0000

    Microservice 1 Added

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 const express = require('express')
 const app = express()
 var port = process.env.PORT || 8080;
-//app.use(express.static('static'))
 app.use (express.urlencoded ({extended: false}))
-const cors = require('cors')//New for Microservice
-app.use(cors())//New for Microservice
-app.listen(port, () => 
-    console.log(`Express server is running on port :${port}`))
+const cors = require('cors')//New for microservice
+app.use(cors())//New for microservice
+app.listen(port ,() =>
+    console.log(`Express server is running on port " :${port}`))
 app.get('/', (req, res) => {
-    res.send('Microservice 1 Gateway by Niharika Gadhave and Hitesh Sakhare. Usage: host/Age_ChineseZodiac?year=xxxx')  
+    res.send("Microservice1 gateway by Niharika Gadhave and Hitesh Sakhare");  
 })
-app.get('/Age_ChineseZodiac', function (req, res) {
-    
-    var birth_year = req.query.year;
-    var current_year = new Date().getFullYear();
-    console.log(`Birth_year: ${birth_year}`);
-    var age = current_year - birth_year;
-    console.log(`Age: ${age}`);
-    var mod = birth_year % 12;
-    var arr = ["Monkey","Rooster","Dog","Pig","Rat","Ox","Tiger","Rabbit","Dragon","Snake","Horse","Goat"];  
-    let animal = arr[mod];
-    res.send(`You are ${age} years old and you are a ${animal}.`);
-
+app.get("/microservice1", (req, res)=>{
+    var birthMonth;
+    var printmonth;
+    var printsoulSymbol;
+    const soulSymbol=["Dragon","Phoenix","Yin-Yang","Lion","Wolf","Fish","Fire","Horse","Flower","Star","Tree","Water"];
+    const month=["January","February","March","April","May","June","July","August","September","October","November","December"];
+    if (birthMonth==req.query.birthMonth){
+      printmonth=month[birthMonth - 1];
+      printsoulSymbol=soulSymbol[birthMonth - 1];
+    }
+    res.send(`Your Birth Month is ${printmonth} and your Soul Symbol is ${printsoulSymbol}.`);
 });
-
