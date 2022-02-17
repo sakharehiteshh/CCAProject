09ac6dc6d0c4fde9ab0eec1951def406380b1566
Niharika Santosh Gadhave <gadhaven1@udayton.edu>
2022-02-17 06:03:03 +0000

    Microservice1 updated

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const express = require('express')
 const app = express()
-var option=5;
-var printmonth;
-var printsoulSymbol;
-const soulSymbol=["Dragon","Phoenix","Yin-Yang","Lion","Wolf","Fish","Fire","Horse","Flower","Star","Tree","Water"];
-const month=["January","February","March","April","May","June","July","August","September","October","November","December"];
-const todaysDate = new Date();
-const currentYear = todaysDate.getFullYear();
-var birthMonth= month-1;
-var url_string = "https://8080-52491bb9-3324-4286-8aa3-7600199969fe.cs-us-east1-pkhd.cloudshell.dev/?authuser=2&birthyear=1991" //window.location.href
-
-if (option==req.query.option){
-    printmonth=month[option];
-    printsoulSymbol=soulSymbol[option];
-}
 var port = process.env.PORT || 8080;
-app.use (express.urlencoded ({extended: true}))
+app.use (express.urlencoded ({extended: false}))
 const cors = require('cors')//New for microservice
 app.use(cors())//New for microservice
-app.listen(port)
-    console.log("Express server is running on port " + port)
+app.listen(port ,() =>
+    console.log(`Express server is running on port " :${port}`))
 app.get('/', (req, res) => {
-    res.send("Your birthmonth is "+printmonth+" and soul symbol is "+printsoulSymbol);  
+    res.send("Microservice1 gateway by Niharika Gadhave and Hitesh Sakhare");  
 })
 app.get("/microservice1", (req, res)=>{
-    res.send("microservice 1 result ="+req.query.data)
-})
+    var birthMonth;
+    var printmonth;
+    var printsoulSymbol;
+    const soulSymbol=["Dragon","Phoenix","Yin-Yang","Lion","Wolf","Fish","Fire","Horse","Flower","Star","Tree","Water"];
+    const month=["January","February","March","April","May","June","July","August","September","October","November","December"];
+    if (birthMonth==req.query.birthMonth){
+      printmonth=month[birthMonth - 1];
+      printsoulSymbol=soulSymbol[birthMonth - 1];
+    }
+    res.send(`Your Birth Month is ${printmonth} and your Soul Symbol is ${printsoulSymbol}.`);
+});
