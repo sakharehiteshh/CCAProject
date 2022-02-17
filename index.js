63dc82c0b7bd11113b5c20c2ed365770818ef40f
Hitesh Vilas Sakhare <sakhareh1@udayton.edu>
2022-02-17 00:15:07 +0000

    microservice 1 updated

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const express = require('express')
 const app = express()
-const zodiac=["Monkey","Rooster","Dog","Pig","Rat","Ox","Tiger","Rabbit","Dragon","Snake","Horse","Goat"];
-const todaysDate = new Date();
-const currentYear = todaysDate.getFullYear(); 
-var url_string = "https://8080-52491bb9-3324-4286-8aa3-7600199969fe.cs-us-east1-pkhd.cloudshell.dev/?authuser=2&birthyear=1998" //window.location.href
-var url = new URL(url_string);
-var c = url.searchParams.get("birthyear");
-var birthyear = parseInt(c);
-var age= currentYear-birthyear;
-var zodiac_option=birthyear%12;
-var zodiac_answer = zodiac[zodiac_option];
-var port = process.env.PORT || 8080;
-app.use (express.urlencoded ({extended: true}))
-const cors = require('cors')//New for microservice
-app.use(cors())//New for microservice
-app.listen(port)
-    console.log("Express server is running on port " + port)
+var port = process.env.PORT || 8080 
+//app.use(express.static('static'))
+app.use (express.urlencoded ({extended: false}))
+app.listen(port, () => 
+    console.log(`Express server is running on port :${port}`))
 app.get('/', (req, res) => {
-    res.send("Your age is "+age+" and chinese zodiac sign is "+zodiac_answer);  
+    res.send('Microservice 1 Gateway by Niharika Gadhave and Hitesh Sakhare. Usage: host/Age_ChineseZodiac?year=xxxx')  
 })
-app.get("/microservice1", (req, res)=>{
-    res.send("microservice 1 result ="+req.query.data)
-})
+app.get('/Age_ChineseZodiac', function (req, res) {
+    var result
+    const zodiac=["Monkey","Rooster","Dog","Pig","Rat","Ox","Tiger","Rabbit","Dragon","Snake","Horse","Goat"];
+    const todaysDate = new Date();
+    const currentYear = todaysDate.getFullYear();
+    var c = res.send.get("year");
+    var birthyear = parseInt(c);
+    var age= currentYear-birthyear;
+    var zodiac_option=birthyear%12;
+    var zodiac_answer = zodiac[zodiac_option];
+    res.send(result)
+});
