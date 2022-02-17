e5b4e9e64eb743f284a6c6145d5bd024b2579bbe
Hitesh Vilas Sakhare <sakhareh1@udayton.edu>
2022-02-17 06:17:09 +0000

    Microservice 1 Added

--- index.js
+++ index.js
@@ -10,14 +10,12 @@ app.get('/', (req, res) => {
     res.send("Microservice1 gateway by Niharika Gadhave and Hitesh Sakhare Usage:host/microservice1?birthMonth=X");  
 })
 app.get("/microservice1", (req, res)=>{
-    var birthMonth;
+    var birthMonth = req.query.birthMonth;
     var printmonth;
     var printsoulSymbol;
     var soulSymbol=["Dragon","Phoenix","Yin-Yang","Lion","Wolf","Fish","Fire","Horse","Flower","Star","Tree","Water"];
     var month=["January","February","March","April","May","June","July","August","September","October","November","December"];
-    if (birthMonth==req.query.birthMonth){
-      printmonth=month[birthMonth - 1];
-      printsoulSymbol=soulSymbol[birthMonth - 1];
-    }
+    printmonth=month[birthMonth - 1];
+    printsoulSymbol=soulSymbol[birthMonth - 1];
     res.send(`Your Birth Month is ${printmonth} and your Soul Symbol is ${printsoulSymbol}.`);
 });
