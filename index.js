0ebaa6631b1afcb3972f94b3ea5044863b8d7423
Niharika Santosh Gadhave <gadhaven1@udayton.edu>
2022-02-17 05:48:15 +0000

    Microservice1 updated

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const app = express()
-const zodiac=["Monkey","Rooster","Dog","Pig","Rat","Ox","Tiger","Rabbit","Dragon","Snake","Horse","Goat"];
+var option=5;
+var printmonth;
+var printsoulSymbol;
+const soulSymbol=["Dragon","Phoenix","Yin-Yang","Lion","Wolf","Fish","Fire","Horse","Flower","Star","Tree","Water"];
+const month=["January","February","March","April","May","June","July","August","September","October","November","December"];
 const todaysDate = new Date();
-const currentYear = todaysDate.getFullYear(); 
+const currentYear = todaysDate.getFullYear();
+var birthMonth= month-1;
 var url_string = "https://8080-52491bb9-3324-4286-8aa3-7600199969fe.cs-us-east1-pkhd.cloudshell.dev/?authuser=2&birthyear=1991" //window.location.href
-var url = new URL(url_string);
-var c = url.searchParams.get("birthyear");
-var birthyear = parseInt(c);
-var age= currentYear-birthyear;
-var zodiac_option=birthyear%12;
-var zodiac_answer = zodiac[zodiac_option];
+
+if (option==req.query.option){
+    printmonth=month[option];
+    printsoulSymbol=soulSymbol[option];
+}
 var port = process.env.PORT || 8080;
 app.use (express.urlencoded ({extended: true}))
 const cors = require('cors')//New for microservice
@@ -17,7 +21,7 @@ app.use(cors())//New for microservice
 app.listen(port)
     console.log("Express server is running on port " + port)
 app.get('/', (req, res) => {
-    res.send("Your age is "+age+" and chinese zodiac sign is "+zodiac_answer);  
+    res.send("Your birthmonth is "+printmonth+" and soul symbol is "+printsoulSymbol);  
 })
 app.get("/microservice1", (req, res)=>{
     res.send("microservice 1 result ="+req.query.data)
