e789037bd4a22cf9f038a0f08716dc9ff8f376c2
Hitesh Vilas Sakhare <sakhareh1@udayton.edu>
2022-02-28 07:21:03 +0000

    Microservice 1 added

--- index.js
+++ index.js
@@ -7,26 +7,17 @@ app.use(cors())//New for microservice
 app.listen(port ,() =>
     console.log(`Express server is running on port " :${port}`))
 app.get('/', (req, res) => {
-    res.send("Microservice1 gateway by Niharika Gadhave and Hitesh Sakhare");  
+    res.send("Microservice1 gateway by Niharika Gadhave and Hitesh Sakhare Usage:host/microservice1?birthMonth=X");  
 })
 app.get("/microservice1", (req, res)=>{
-    var birthMonth;
+    var birthMonth = req.query.birthMonth;
     var printmonth;
     var printsoulSymbol;
-<<<<<<< HEAD
     var soulSymbol=["Dragon","Phoenix","Yin-Yang","Lion","Wolf","Fish","Fire","Horse","Flower","Star","Tree","Water"];
     var month=["January","February","March","April","May","June","July","August","September","October","November","December"];
 
     printmonth=month[birthMonth - 1];
     printsoulSymbol=soulSymbol[birthMonth - 1];
     
-=======
-    const soulSymbol=["Dragon","Phoenix","Yin-Yang","Lion","Wolf","Fish","Fire","Horse","Flower","Star","Tree","Water"];
-    const month=["January","February","March","April","May","June","July","August","September","October","November","December"];
-    if (birthMonth==req.query.birthMonth){
-      printmonth=month[birthMonth - 1];
-      printsoulSymbol=soulSymbol[birthMonth - 1];
-    }
->>>>>>> 9c2521528a8e28c5d94720104159fcad744a3c7d
     res.send(`Your Birth Month is ${printmonth} and your Soul Symbol is ${printsoulSymbol}.`);
 });
