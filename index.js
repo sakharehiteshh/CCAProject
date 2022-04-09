272e03b02ca33a33bab570d4ce653784f6793a01
Hitesh Vilas Sakhare <sakhareh1@udayton.edu>
2022-04-09 05:50:25 +0000

    update

--- index.js
+++ index.js
@@ -5,19 +5,17 @@ app.use (express.urlencoded ({extended: false}))
 const cors = require('cors')//New for microservice
 app.use(cors())//New for microservice
 app.listen(port ,() =>
-    console.log(`Express server is running on port " :${port}`))
+console.log(`Express server is running on port " :${port}`))
 app.get('/', (req, res) => {
-    res.send("Microservice1 gateway by Niharika Gadhave and Hitesh Sakhare");  
+    res.send("Microservice1 gateway by Niharika Gadhave and Hitesh SakhareUsage:host/microservice1?birthMonth=X");
 })
 app.get("/microservice1", (req, res)=>{
-    var birthMonth;
+    var birthMonth = req.query.birthMonth;
     var printmonth;
     var printsoulSymbol;
-    const soulSymbol=["Dragon","Phoenix","Yin-Yang","Lion","Wolf","Fish","Fire","Horse","Flower","Star","Tree","Water"];
-    const month=["January","February","March","April","May","June","July","August","September","October","November","December"];
-    if (birthMonth==req.query.birthMonth){
-      printmonth=month[birthMonth - 1];
-      printsoulSymbol=soulSymbol[birthMonth - 1];
-    }
+    var soulSymbol=["Dragon","Phoenix","Yin-Yang","Lion","Wolf","Fish","Fire","Horse","Flower","Star","Tree","Water"];
+    var month=["January","February","March","April","May","June","July","August","September","October","November","December"];
+    printmonth=month[birthMonth - 1];
+    printsoulSymbol=soulSymbol[birthMonth - 1];
     res.send(`Your Birth Month is ${printmonth} and your Soul Symbol is ${printsoulSymbol}.`);
 });
