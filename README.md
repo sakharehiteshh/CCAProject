d1bf6b0f2fd0639d86d4960830eafc1ca427d06c
Hitesh Vilas Sakhare <sakhareh1@udayton.edu>
2022-05-06 01:35:21 +0000

    update

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
-# CCA - Cloud Computing and Applications
-### CCA-Project
-Name: Hitesh Vilas Sakhare
-Email: <sakhareh1@udayton.edu> 
-Name: Niharika Santosh Gadhave
-Email: <gadhaven1@udayton.edu>
+# README #
+
+This README would normally document whatever steps are necessary to get your application up and running.
+
+### What is this repository for? ###
+
+* Quick summary
+* Version
+* [Learn Markdown](https://bitbucket.org/tutorials/markdowndemo)
+
+### How do I get set up? ###
+
+* Summary of set up
+* Configuration
+* Dependencies
+* Database configuration
+* How to run tests
+* Deployment instructions
+
+### Contribution guidelines ###
+
+* Writing tests
+* Code review
+* Other guidelines
+
+### Who do I talk to? ###
+
+* Repo owner or admin
+* Other community or team contact
--- index.js
+++ index.js
@@ -1,49 +1,53 @@
 const express= require('express');
 const app=express();
-const port = process.env.PORT || 8080; 
+var port = process.env.PORT || 8080; 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-server.listen(port); //changed from app.listen(port)
+server.listen(port);
 app.use(express.static('static'));
-app.use(express.urlencoded({extended: false}));
-// const superagent = require('superagent')
+app.use(express.json({extended: false}));
+const superagent = require('superagent')
 console.log("WebChat server is running on port " + port);
 const MongoClient = require('mongodb').MongoClient;
-// const mongourl = "mongodb+srv://cca-gadhaven1:CCAlab2022@cca-gadhaven1.kmct8.mongodb.net/cca-labs?retryWrites=true&w=majority"; //the full URL with username/password
-// const dbClient = new MongoClient (mongourl, {useNewUrlParser: true, useUnifiedTopology: true});
-// dbClient.connect (err => {
-//     if (err) throw err;
-//     console.log ("Connected to the MongoDB cluster");
-// });
+const mongourl = "mongodb+srv://cca-gadhaven1:CCAlab2022@cca-gadhaven1.kmct8.mongodb.net/cca-project-team16?retryWrites=true&w=majority";
+const dbClient = new MongoClient(mongourl,
+                {useNewUrlParser: true, useUnifiedTopology: true});
+dbClient.connect(err => {
+    if (err) throw err;
+    console.log("Connected to the MongoDB cluster");
+});
+// app.use(cors())
 app.get("/", (req,res)=>{
-    res.sendFile(__dirname + '/static/assignment.html')
+    res.sendFile(__dirname + '/static/chatclient.html')
 })
 
-// app.get("/chatbot", (req,res)=>{
-//     res.sendFile(__dirname + '/static/chatbot.html')
-// })
+app.get("/chatbot", (req,res)=>{
+    res.sendFile(__dirname + '/static/chatbot.html')
+})
 
 
 function signup(account,callback){
-    // superagent.post('https://cca-gadhaven1-accountsearch.herokuapp.com/signup')
-    // .send(account)
-    // .end((err, res) => {
+    superagent.post('https://cca-team16-mschatdatabase.herokuapp.com/signup/')
+    .send(account)
+    .end((err, res) => {
         if (res.body.status==="Registered"){
         return callback(true,res.body.message,null);
         }else
-        callback(false,res.body.message,null);
-        }
+        callback(false,res.body.message,null);  
+    }
+)}  
     
 
 function login(username,password, callback){
-    // superagent.get(`https://cca-gadhaven1-accountsearch.herokuapp.com/login/${username}/${password}`)
-    // .end((err, res) => {
+    superagent.get(`https://cca-team16-mschatdatabase.herokuapp.com/login/${username}/${password}`)
+    .end((err, res) => {
+        console.log(res.body,err)
         if (res.body.status==="found"){
-        return callback(true,res.body.message,res.body.account);
+        return callback(true,null,res.body.account);
         }else
         callback(false,res.body.message,null);
         }
-    
+    )}
 
 function BroadcastAuthenticatedClients(event,message){
 var sockets = io.sockets.sockets;
@@ -53,6 +57,8 @@ if(socketclient && socketclient.authenticated){
 socketclient.emit(event,message);
 }
 }}
+
+
 
 io.on('connection', (socketclient) => { 
 	console.log('A new client is connected!'); 
@@ -71,22 +77,67 @@ io.on('connection', (socketclient) => {
         var onlineClients = Object.keys(io.sockets.sockets).length;
         var byemessage = `${socketclient.id} is disconnected! Number connected clients: ${onlineClients}`;
         console.log(byemessage);
-        BroadcastAuthenticatedClients("online", byemessage);
+        // BroadcastAuthenticatedClients("online", byemessage);
+        var sockets = io.sockets.sockets; 
+            let onlineUsers = new Map(); 
+            for (var id in sockets){ 
+                const socketclient = sockets[id]; 
+                if(socketclient && socketclient.authenticated && socketclient.profile){ 
+                    onlineUsers.set(socketclient.username,socketclient.profile); } } 
+                    let onlineUsersJSON = []; 
+                    onlineUsers.forEach((value,key)=>{ 
+                    onlineUsersJSON.push(value); });
+                    for (var id in sockets){ 
+                        const socketclient = sockets[id]; 
+                        if(socketclient && socketclient.authenticated && socketclient.profile){ 
+                            socketclient.emit("update", onlineUsersJSON); } }
         });
+
        socketclient.on("login", (username,password) => {
-            console.log(`Debug> Login data: ${username}/${password}`);
+            console.log(`-pr> Login data: ${username}/${password}`);
             login(username,password,(authenticated,message,account)=>{
-            if (authenticated){
+            //console.log(authenticated,message,account);
+                if (authenticated){
             socketclient.authenticated=true;
-            socketclient.emit("authenticated", account.username);
+            socketclient.username=account.username; 
+            socketclient.profile=account
+            socketclient.emit("authenticated", account);
             console.log(`Debug> ${username} is authenticated and logged in successfully`);
-            }else{
-            socketclient.emit("login-failed",message);
-            console.log(`Debug> Login failed: ${username}/${password}`);
-            }
-            })
+            var sockets = io.sockets.sockets; 
+            let onlineUsers = new Map(); 
+            for (var id in sockets){ 
+                const socketclient = sockets[id]; 
+                if(socketclient && socketclient.authenticated && socketclient.profile){ 
+                    onlineUsers.set(socketclient.username,socketclient.profile); } } 
+                    let onlineUsersJSON = []; 
+                    onlineUsers.forEach((value,key)=>{ 
+                        onlineUsersJSON.push(value); }); 
+                        console.log(onlineUsersJSON) 
+                        for (var id in sockets){ 
+                            const socketclient = sockets[id]; 
+                            if(socketclient && socketclient.authenticated && socketclient.profile){ 
+                                socketclient.emit("userlist", onlineUsersJSON); 
+                            } 
+                        }
+
+                }else{
+                    socketclient.emit("login-failed",message);
+                    console.log(`Debug> Login failed: ${username}/${password}`);
+                    }
+                })
             });
 
+        socketclient.on("privatechat" , 
+        (receiver,message, sender) => {var sockets = io.sockets.sockets;
+        for (var id in sockets){ 
+        let socketclient = sockets[id]; 
+        if(socketclient && socketclient.authenticated && 
+            socketclient.username==receiver){ 
+            socketclient.emit("privatechat" ,
+            sender,message); 
+        } 
+    }})    
+        
         socketclient.on("register", (user) => {
             console.log(`Debug> Login data: ${user}`);
             signup(user,(authenticated,message,account)=>{
